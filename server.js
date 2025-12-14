dcca9b8fcda313aa8c7cccd112f49e70868cdb42
Felipe <felipegustavo042009@gmail.com>
2025-12-13 23:59:48 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname, 'biuld'))
+app.use(express.static(__dirname, 'build'))
 
 const PORT = 5000;
 
@@ -817,4 +817,5 @@ app.patch('/AtualizarReserva', async (req, res) => {
 // INICIA SERVIDOR 
 app.listen(PORT, () => {
     console.log(`Servidor rodando com sucesso na porta ${PORT}`);
+
 });
